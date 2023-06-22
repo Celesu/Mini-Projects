8d7e0cdd8faf7d5e249cfeb2706d3c3670daf9f9
Celesu <50899024+Celesu@users.noreply.github.com>
2023-06-22 12:57:25 +0700

    Update code_generator.py
    I wanna improve the speed (and the lines hahaha)

--- consecutive_code_generator/code_generator.py
+++ consecutive_code_generator/code_generator.py
@@ -1,22 +1,23 @@
 def code_generator(last_code=""):
   code_str = 'bcdfghjklmnpqrstvwxyz0123456789'
-
-  # kalau sama
-  if all([1 if char_ == '9' else 0 for char_ in last_code]):
-    return 'b'*(len(last_code)+1) # new_codenya
+  
+  if last_code == "":
+    return 'b'
 
   for i in range(len(last_code)-1, -1, -1):
     if last_code[i] != '9':
       last_code = last_code[:i] + code_str[code_str.index(last_code[i]) + 1] + last_code[i + 1:]
       break
     
-    last_code = last_code[:i] + 'b' + last_code [i + 1:]
-      
+    last_code = last_code[:i] + 'b' + last_code[i + 1:]
+    if i == 0:
+      last_code = 'b' + last_code
+        
   return last_code
 
 
-print(code_generator('333b93177'))
-print(code_generator('89'))
+print(code_generator('89933'))
+print(code_generator('999'))
 print(code_generator('789'))
 print(code_generator(''))
 print(code_generator('997899'))
